499edb9390a5fe2b60921ad8ec938614ea1efc93
prajwagmode <pratikwaghmode@MacBookAir.lan>
2025-10-28 12:35:17 +0530

    fix: valid vite config and inline import for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  root: '.',
   build: {
     outDir: 'dist',
-  },
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 })
-
